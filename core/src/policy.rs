531d38a19d06afc7d22dc07148f8cf4e9595c44b
Liotitch <rph.vaillant@gmail.com>
2017-02-23 21:14:20 +0100

    u32 policy

--- core/src/policy.rs
+++ core/src/policy.rs
@@ -8,8 +8,12 @@
 #[cfg(feature = "u16_handle")]
 pub use self::u16_handle::*;
 
+#[cfg(feature = "u32_handle")]
+pub use self::u32_handle::*;
+
 pub type IdSet = ::bit_set::BitSet;
 
+#[cfg(feature = "u16_handle")]
 mod u16_handle {
     /// The id type for an entity
     pub type Id = u16;
@@ -29,5 +33,28 @@ mod u16_handle {
     #[inline]
     pub fn id_from_usize(value: usize) -> Id {
         value as u16
+    }
+}
+
+#[cfg(feature = "u32_handle")]
+mod u32_handle {
+    /// The id type for an entity
+    pub type Id = u32;
+
+    /// The version type for an entity
+    pub type Version = u32;
+
+    /// Returns the maxmimum of entities that can exists at the same time
+    pub fn max_entity_count() -> usize {
+        use std::u32::MAX;
+        return MAX as usize;
+    }
+
+    /// Converts a usize to an Id.
+    ///
+    /// **Panics** if the usize overflows the `Id`
+    #[inline]
+    pub fn id_from_usize(value: usize) -> Id {
+        value as u32
     }
 }
